d1e137d87697d68b4d91cef1e35e413c2e14059e
varungajula07072002-collab <varungajula07072002@gmail.com>
2026-07-26 11:36:19 +0530

    Rename script.js to Xanora.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{e.preventDefault();document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});}));
